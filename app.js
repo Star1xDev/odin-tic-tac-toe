2809ed3a016d2355ef0c1f20ac1bdc98356f5a5d
Star1xDev <abdastar2@gmail.com>
2022-08-29 18:03:36 +0100

    changed and added some functions
    changed the gameboard render function to render the svgs instead of text

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 //---------- gameboard module -------------//
 
 const gameboard = (function(){
+  const cellss = document.querySelectorAll(".cell");
   const cells = Array.from(document.querySelectorAll(".cell"));
   let board = ["","","",
                "","","",
@@ -9,8 +10,18 @@ const gameboard = (function(){
 
   // function to render the board
   function render(){
+    console.log("in render");
+
     cells.forEach( function(cell, index) {
-          cell.textContent = board[index];
+
+          // cell.textContent = board[index];
+          if (board[index] === "x") {
+            cell.classList.add("showx");
+          }
+          if (board[index] === "o") {
+            cell.classList.add("showo");
+          }
+
     });
   }
 
@@ -18,6 +29,7 @@ const gameboard = (function(){
   // add event listeners on each cell
   cells.forEach(function(cell,index){
     cell.addEventListener("click", ()=>{
+        // console.log(game.currentplayer.name);
 
         // add player marker to board
         board[index] = game.currentplayer.marker;
@@ -65,13 +77,59 @@ const playerFactory = function(name, marker){
 
 const game = (function(){
 
-  const saveBtns = document.querySelectorAll(".save");
-  const playerOneInput = document.getElementById("playerone");
-  const playerTwoInput = document.getElementById("playertwo");
-  // create players
-  let playerOne = playerFactory("star", "x");
-  let playerTwo = playerFactory("jin", "o");
+  // function start game
+  function startGame(){
+    const startBtn = document.querySelector(".start")
+    const gameboard = document.querySelector(".gameboard");
+    startBtn.addEventListener("click", ()=>{
 
+      if (!gameboard.classList.contains("show")) {
+        gameboard.classList.add("show");
+      }
+    })
+  }
+  startGame();
+
+  // create players
+  let playerOne = playerFactory("playerone", "x");
+  let playerTwo = playerFactory("playertwo", "o");
+
+
+  function updatePlayers(){
+    const saveBtns = document.querySelectorAll(".save");
+    const playerOneInput = document.getElementById("playerone");
+    const playerTwoInput = document.getElementById("playertwo");
+    saveBtns[0].addEventListener("click", ()=>{
+        console.log("updating playerone");
+        // game.playerOne = playerFactory(playerOneInput.value,"x");
+        let playerone = playerFactory(playerOneInput.value,"x");
+        game.setPlayerOne(playerone)
+        playerOneInput.value = "";
+        console.log(game.playerOne);
+        console.log(game.currentplayer);
+
+    })
+    saveBtns[1].addEventListener("click", ()=>{
+        console.log("updating playerone");
+        // game.playerTwo = playerFactory(playerTwoInput.value,"o");
+        let playertwo = playerFactory(playerTwoInput.value,"o");
+        game.setPlayerTwo(playertwo);
+        playerTwoInput.value = "";
+        console.log(game.playerTwo);
+
+    })
+  }
+
+  updatePlayers();
+
+  // function to set players
+  function setPlayerOne(playerone){
+    this.playerOne = playerone;
+    this.currentplayer = playerone;
+  }
+  function setPlayerTwo(playertwo){
+    this.playerTwo = playertwo;
+  }
 
   // set starting point
   let currentplayer = playerOne;
@@ -98,29 +156,40 @@ const game = (function(){
                 console.log('winner!');
                 alert(`${this.currentplayer.name} wins!`);
                 this.winnerDeclared = true;
-                resetGame();
+                this.resetGame();
             }
         })
+
     }
 
   // next player
   function nextTurn(){
-    if (this.currentplayer === playerOne) {
-      this.currentplayer = playerTwo;
+    if (this.currentplayer === this.playerOne) {
+      this.currentplayer = this.playerTwo;
     } else{
-      this.currentplayer = playerOne;
+      this.currentplayer = this.playerOne;
     }
   }
 
   // reset the game
   function resetGame(){
     this.currentplayer = playerOne;
+    this.playerOne = playerOne;
+    this.playerTwo = playerTwo;
     this.winnerDeclared = false;
     this.remainingSpots = 9;
+    const cells = Array.from(document.querySelectorAll(".cell"));
+    cells.forEach( function(cell) {
+      cell.style.pointerEvents = "auto";
+    });
     gameboard.board.forEach( function(elm,index, arr) {
       arr[index] = "";
     });
     gameboard.render();
+    console.log("after reset");
+    console.log(game.playerOne);
+    console.log(game.playerTwo);
+    console.log(game.currentplayer);
   }
 
 
@@ -130,14 +199,20 @@ const game = (function(){
     remainingSpots,
     nextTurn,
     checkWinner,
-    resetGame
+    resetGame,
+    playerOne,
+    playerTwo,
+    setPlayerOne,
+    setPlayerTwo
   }
 
 })();
 
 
-// console.log(game);
-// console.log(playerOne);
-// console.log(playerTwo);
-
+console.log("in start");
+console.log(game.playerOne);
+console.log(game.playerTwo);
+const cell = document.querySelector(".cell");
+console.log(cell);
+console.log(cell.firstElementChild);
 
